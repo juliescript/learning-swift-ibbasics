32cfb37c4367ef45dba460aceb931ce409e79b2c
Julieta Campos <julieicelie@gmail.com>
2020-09-07 16:59:57 +0200

    Initial Commit

---
+++ IBBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBBasics
+//
+//  Created by Julieta Cristina Campos Guzmán on 07.09.20.
+//  Copyright © 2020 Julieta Cristina Campos Guzmán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
