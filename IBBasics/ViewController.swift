74a3e41dee6b6693c3ed96efe892c13e51bf28ec
Julieta Campos <julieicelie@gmail.com>
2020-09-07 18:35:04 +0200

    Fix bug

--- IBBasics/ViewController.swift
+++ IBBasics/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
     @IBOutlet var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        myButton.setTitleColor(.red, for: .normal)
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func buttonPressed(_ sender: Any) {
-        print("The button was pressed")
-    }
     
+    @IBAction func changeTitle(_ sender: Any) {
+        mainLabel.text = "This app rocks!"
+    }
 }
 
